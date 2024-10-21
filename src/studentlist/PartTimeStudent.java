0896cd6fcd29d8a8f34af6f743e055bfb52a73ff
Anmol Pal Singh <sin15077@sheridancollege.ca>
2024-10-21 12:38:15 -0400

    Added getter and setter for PartTimeStudent branch

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author user
+ */
+public class PartTimeStudent extends Student {
+
+    
+       
+
+   
+    public String getNumCourses() {
+        return NumCourses;
+    }
+
+    
+    public void setNumCourses(String NumCourses) {
+        this.NumCourses = NumCourses;
+    }
+
+}
